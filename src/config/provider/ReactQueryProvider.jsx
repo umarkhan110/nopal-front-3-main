cef8ae6a438939ea61f53bf47e33bd6fc08d7f4d
umarkhan110 <umardraz277@gmail.com>
2024-05-08 21:55:00 +0500

    home page done

--- src/config/provider/ReactQueryProvider.jsx
+++ src/config/provider/ReactQueryProvider.jsx
@@ -1,18 +1,29 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      // refetchOnMount: false,
-      retry: 1,
-    },
-  },
-});
+// const queryClient = new QueryClient({
+//   defaultOptions: {
+//     queries: {
+//       refetchOnWindowFocus: false,
+//       // refetchOnMount: false,
+//       retry: 1,
+//     },
+//   },
+// });
 
 const ReactQueryProvider = ({ children }) => {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+          },
+        },
+      })
+  );
   return (
     <>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
